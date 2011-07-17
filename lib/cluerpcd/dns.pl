5a1657388dd39d4a192a6ccc12874d21be2de6dc
Mantas Mikulėnas <grawity@nullroute.eu.org>
2011-07-18 00:49:22 +0300

    Add dns management

---
+++ lib/cluerpcd/dns.pl
@@ -0,0 +1,11 @@
+#!perl
+dns => sub {
+	my ($self, $req) = @_;
+
+	unless ($self->{authed}) {
+		return {failure,
+			msg => "access denied"};
+	}
+
+	return $self->spawn_helper("rd-dns", $req);
+};
