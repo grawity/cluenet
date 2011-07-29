592c7c24817d78b2a4e9b7f90dffa2f0a5dfae5c
Mantas Mikulėnas <grawity@nullroute.eu.org>
2011-07-29 03:07:05 +0300

    LDAP: What the fucking fuck.

--- perl5/Cluenet/LDAP.pm
+++ perl5/Cluenet/LDAP.pm
@@ -62,10 +62,7 @@ sub connect_auth {
 		or croak "$!";
 	$ldap->start_tls(verify => "require", cafile => "/etc/ssl/certs/Cluenet.pem")
 		or croak "$!";
-	my %cb = (
-		user => \getlogin,
-	);
-	my $sasl = Authen::SASL->new(mech => "GSSAPI", callback => \%cb);
+	my $sasl = Authen::SASL->new(mech => "GSSAPI");
 	my $saslclient = $sasl->client_new("ldap", getfqdn(LDAP_MASTER));
 	my $msg = $ldap->bind(sasl => $saslclient);
 	$msg->code and die "error: ".$sasl->error;
