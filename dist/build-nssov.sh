235ca1ca30b6c566fdf056800a411dc6d754f318
Mantas Mikulėnas <grawity@nullroute.eu.org>
2012-02-18 17:47:59 +0200

    initial commit

---
+++ dist/build-nssov.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+version='2.4.29'
+
+download() {
+	cd "$buildroot"
+	url="ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$version.tgz"
+	wget -c "$url"
+	tar xzf "${url##*/}"
+}
+
+build() {
+	cd "$buildroot/openldap-$version"
+	./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var
+	make depend
+	cd contrib/slapd-modules/nssov
+	make
+}
+
+package() {
+	cd "$buildroot/openldap-$version"
+	cd contrib/slapd-modules/nssov
+	make DESTDIR="${destdir%/}/" moduledir="." schemadir="." install
+	echo "Installed to $destdir"
+}
+
+buildroot="$PWD/openldap-nssov-build"
+
+if (( UID )); then
+	mkdir -p "$buildroot"
+	download
+	build
+	destdir="$buildroot/nssov"
+	package
+	echo "Run $0 as root to install system-wide."
+else
+	destdir="/cluenet/lib/nssov"
+	package
+fi
