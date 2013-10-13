c31bb9346b69e13f48f41d5fb6d49e0644a65c74
Mantas Mikulėnas <grawity@gmail.com>
2013-10-13 03:39:34 +0300

    allow some constatns to be overridden

--- lib/perl5/Cluenet.pm
+++ lib/perl5/Cluenet.pm
@@ -4,13 +4,13 @@ use strict;
 
 =head1 Cluenet
 
-Constants and variables aren't exported – they're to be used as Cluenet::FOO
-and $Cluenet::FOO.
+Constants and variables intentionally aren't exported.
 
 =cut
 
-our $LDAP_HOST = "ldap://ldap.cluenet.org";
-our $KADM_HOST = "krb5-admin.cluenet.org";
+our $LDAP_HOST = $ENV{CN_LDAP_SERVER} // "ldap://ldap.cluenet.org";
+our $LDAP_BASE = $ENV{CN_LDAP_BASE} // "dc=cluenet,dc=org";
+our $KADM_HOST = $ENV{CN_KADM_HOST} // "krb5-admin.cluenet.org";
 our $API_PRINC = undef;
 our $API_KEYTAB = undef;
 
